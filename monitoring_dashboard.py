ee56aded019f05a97644a7e8e5e40e6a56390fa4
SB-BH <129633319+S00013@users.noreply.github.com>
2023-07-27 00:41:45 +0100

    Update monitoring_dashboard.py

--- monitoring_dashboard.py
+++ monitoring_dashboard.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, abort
 import requests
 import jwt
 import configparser
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 API_URL = 'http://127.0.0.1:5000'
 
+# Read configuration from config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Load authentication secret key
+SECRET_KEY = config.get('AUTHENTICATION', 'SECRET_KEY')
+
+# Define token expiration time (in minutes)
+TOKEN_EXPIRATION_MINUTES = config.getint('AUTHENTICATION', 'TOKEN_EXPIRATION_MINUTES')
+
 def get_token():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    SECRET_KEY = config.get('AUTHENTICATION', 'SECRET_KEY')
     expiration_time = datetime.utcnow() + timedelta(minutes=TOKEN_EXPIRATION_MINUTES)
     payload = {'exp': expiration_time}
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
@@ -27,9 +35,9 @@ def get_status():
         headers = {'Authorization': f'Bearer {token}'}
         response = requests.get(f'{API_URL}/status', headers=headers)
         response.raise_for_status()
-        return response.json(), 200
+        return jsonify(response.json()), 200
     except requests.exceptions.RequestException as e:
-        return {'error': str(e)}, 500
+        return jsonify({'error': 'Failed to fetch status.'}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
